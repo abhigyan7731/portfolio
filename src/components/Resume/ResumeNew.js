f94b5f2627a57f53a4c3fe29ce49f703cfccdd4d
ABHIGYAN KUMAR GUPTA <abhigyankumar268@gmail.com>
2025-04-15 17:14:51 +0530

    resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "./src/Assets/resume.pdf";
+import pdf from "file:///C:/Users/abhig/protofolio%203.0/vcard-personal-portfolio/Portfolio/src/Assets/resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
